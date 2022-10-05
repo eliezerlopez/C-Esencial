756f2884f2ca47f4e82745242f375b976fd8d118
Eliezer López <eliezer.lopezrodriguez@alum.uca.es>
2022-10-05 15:32:37 +0000

    Nuevo ejercicio sobre comparación de enteros
    Comprobar si dos numeros enteros son iguales

---
+++ comprobacionNumerosEnterosIguales.c
@@ -0,0 +1,16 @@
+// Completa el siguiente programa para comprobar
+// si dos numeros enteros introducios por
+// el usuario son iguales
+
+#include <stdio.h>
+
+int main(){
+    
+    int numero1, numero2;
+    
+    // lectura numeros
+    // comparacion
+    // mostrar resultado en pantalla
+    
+    return 0;
+}
